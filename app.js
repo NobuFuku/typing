e288d44d573f701f0459c21303e2bc38aff1353b
NobuFuku <67360481+NobuFuku@users.noreply.github.com>
2020-10-30 15:30:34 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var p = document.getElementById('text');
+
+//タイピングする文字列をここで用意しておく
+var textLists = [
+    'Hello World',
+    'This is my App',
+    'How are you?',
+    'Hello Hello',
+    'I love JavaScript!',
+    'Good morning',
+    'I am Japanese',
+    'Let it be'
+];
+var checkTexts = [];
