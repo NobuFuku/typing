d700ce74a96e35155e0ff9fcd456d72135f67023
NobuFuku <67360481+NobuFuku@users.noreply.github.com>
2020-10-31 03:01:42 +0900

    1

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ var textLists = [
     'Let it be'
 ];
 var checkTexts = [];
+
+createText();
+
+function createText() {
+    //文字列をランダムに取得する
+    var rnd = Math.floor(Math.random() * textLists.length);
+
+    //前の文字列を削除してから次の文字列を表示する
+    p.textContent = '';
+    
+    //文字列を1文字ずつに分解して、それぞれにspanタグを挿入する
+    checkTexts = textLists[rnd].split('').map(function(value) {
+        var span = document.createElement('span');
+
+        span.textContent = value;
+        p.appendChild(span);
+
+        return span;
+    });
+}
